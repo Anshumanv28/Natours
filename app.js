2a8090267bf9a93f0a4a1085080446ff5f692fdb
Anshumanv36 <anshumanmishra.v36@gmail.com>
2023-09-28 01:59:30 +0530

    worked on PATCH command

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ app.use(express.json()); //middleware
 // });
 
 const tours = JSON.parse(
+  //JSON file in array format
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req, res) => {
+  //GET all
   //get request can send data from server to the client
   //always specify the version of the api to prevent breaking someones code if you change the verion later on(can also do this in the subdomain)
   res.status(200).json({
@@ -33,6 +35,7 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.get('/api/v1/tours/:id/:x?', (req, res) => {
+  //GET for specific tour
   //created a variable(id) in the url using : (:id/:var/:x fro multiple) these variables are called params
   //added an optional parameter :x? using (?) needn't specify this in the request
   console.log(req.params); //to access the params
@@ -48,8 +51,7 @@ app.get('/api/v1/tours/:id/:x?', (req, res) => {
   }
   //note you could have handled this error in another way too(think with your problem solving skills😉)
 
-  const tour = tours.find((el) => el.id === id); //find the elements with the same id as the params
-
+  const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: 'success',
     data: {
@@ -87,6 +89,40 @@ app.post('/api/v1/tours', (req, res) => {
   //but remember not to send two responses
 });
 //note that we have access access to the newly created tours after each time the server restarts which it luckily does each time we write and save to the tours-simple.JSON file
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  //can also use PUT but why do extra work? so we using PATCH
+
+  const id = req.params.id * 1; //using the second way to handle error
+  const tour = tours.find((el) => el.id === id); //find the elements with the same id as the params
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  // var temp_tours = JSON.stringify(tours);
+  // var temp_tour = tour;
+  // var k1, k2;
+  // const body = JSON.parse(JSON.stringify(req.body));
+  // for (k1 in body) {
+  //   for (k2 in tour) {                                //just trying to update
+  //     if (k1 === k2) {                                //after almost 2hrs of trying I conclude it takes more work both computing and coding wise to patch by changing in between the JSON file
+  //       tour[k1] = `${body[k2]}`;                     //better to just create a new object and delete the older one and then insert the new one
+  //     }
+  //   }
+  // }
+  // temp_tours.replace(temp_tour, JSON.stringify(tour));
+
+  var arr = res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
