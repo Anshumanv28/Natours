2b469152b705f35d8dc3431b532781ba77672e89
Anshumanv36 <anshumanmishra.v36@gmail.com>
2024-09-10 23:35:57 +0530

    added dynamica calculation of average rating on tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel'); //will be used to persist the average rating in the database
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,6 +48,38 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+//static method
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    //"this" points to the model so we can call aggregate on the model
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 }, //number of ratings, for each review we add 1
+        avgRating: { $avg: '$rating' }, //average rating, for each review we add the rating and find the new average
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+//this code will run in sequence as is the order of the code & we can't move the code to after the model is created because then the model will not have the calcAverageRatings method
+reviewSchema.post('save', function (next) {
+  //using "post" middleware because we want to run this after the document is saved
+  //"this" points to the current review
+  // this.calcAverageRatings(this.tour); //this will not work because this points to review and it is not created yet
+  this.constructor.calcAverageRatings(this.tour); //this will work because the constructor is basically the model that created the document
+  // next();  //post middleware does not have access to next()
 });
 
 const Review = mongoose.model('Review', reviewSchema);
