c37787d0828e67f38b909d0c105a336fcef1d9c5
Anshumanv36 <anshumanmishra.v36@gmail.com>
2024-09-11 23:28:28 +0530

    added review statics updation on updation and deletion

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -67,10 +67,17 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
   ]);
   console.log(stats);
 
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 //this code will run in sequence as is the order of the code & we can't move the code to after the model is created because then the model will not have the calcAverageRatings method
@@ -80,6 +87,24 @@ reviewSchema.post('save', function (next) {
   // this.calcAverageRatings(this.tour); //this will not work because this points to review and it is not created yet
   this.constructor.calcAverageRatings(this.tour); //this will work because the constructor is basically the model that created the document
   // next();  //post middleware does not have access to next()
+});
+
+//findByIdAndUpdate(shorthand for findOneAndUpdate)
+//findByIdAndDelete
+
+//remember the API call may return the document before the post middleware runs i.e the document is saved(updated with the data)
+//use 'this' to pass data between pre and post middleware as any this keyword in the pre middleware will be available in the post middleware
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // const r = await this.findOne();
+  this.r = await this.findOne();
+  // console.log(r);  /will not work
+  // console.log(this.r);  //will work
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); //does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
